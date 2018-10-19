fcb262bf980c9b4085d9e50e37199b5cef2f07f9
EMILIAS <emilia.streng@gmail.com>
2018-10-19 16:14:11 +0300

    Chat view

---
+++ ChatSovellus/Controllers/ChatController.cs
@@ -0,0 +1,101 @@
+﻿using ChatSovellus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatSovellus.Controllers
+{
+    public class ChatController : Controller
+    {
+        ChatDBModel db = new ChatDBModel();
+
+        [HttpPost]
+        // GET: Chat
+        public ActionResult Index()
+        {
+            IEnumerable<Message> viestit = db.Message.ToList();     
+
+
+            return View(viestit);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Kommentti()
+        {
+            return View();
+        }
+
+        // GET: Chat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Chat/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Chat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
